e55a270813371b51ba9024c9c2e443f9056aee8f
BuildTools <damian.contreras036@gmail.com>
2023-07-10 02:34:15 -0400

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola esta es Mi App!</Text>
+      <Text>Hola Mundo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#41bf4a',
     alignItems: 'center',
     justifyContent: 'center',
   },
